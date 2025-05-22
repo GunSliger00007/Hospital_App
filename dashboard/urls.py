75e9c9b3cec54ce1e66badc4ac9cff3d1fe0db33
GunSliger00007 <chaudharygehendra46@gmail.com>
2025-05-22 21:08:57 +0545

    fixed url error

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,10 +9,7 @@ urlpatterns = [
     path('logout/',logout_view, name='logout'),
     path('signup/',signup, name='signup'),
     path('profile/', profile_view, name='profile'),
-    path('post/', post_blog, name='post_blog'),
-    path('post/delete/<int:post_id>/', delete_post, name='delete_post'),
-    path('post/update/', update_post, name='update_post'),
-
+   
     path('book/', book_appointment, name='book_appointment'),
     # path('update-appointment/', update_appointment, name='update_appointment'),
     # path('confirm/<int:appointment_id>/', views.confirm_appointment, name='confirm_appointment'),
